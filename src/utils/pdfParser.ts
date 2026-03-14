8b1b57b44dd405ee3624d70d93cf2445c197797b
Jacob Mathew <75663388+mathewjm22@users.noreply.github.com>
2026-03-14 09:24:38 -0600

    Add files via upload

--- src/utils/pdfParser.ts
+++ src/utils/pdfParser.ts
@@ -1,4 +1,4 @@
-import * as pdfjsLib from 'pdfjs-dist';
+import * as pdfjsLib from 'pdfjs-dist/build/pdf';
 import { ExpenseCategory, Transaction } from '../types';
 import { uid } from '../utils';
 
